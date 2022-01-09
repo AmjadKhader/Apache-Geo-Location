4346b45c9bc3d057bd1cfb87c6354f53c35fdcf9
Amjad Khader <Amjadkhader49@gmail.com>
2022-01-09 15:15:40 +0200

    Update index directory path

--- src/main/java/demos/lucene/constants/Constants.java
+++ src/main/java/demos/lucene/constants/Constants.java
@@ -5,9 +5,9 @@ public class Constants {
     public enum eAnalyzerType {STANDARD, SIMPLE, WHITE_SPACE}
     public enum eOperation {ADD, ADD_DOC, UPDATE_DOC, DELETE_DOC}
 
-    public static final String INDEX_DIR_STANDARD = "/home/amjad/Desktop/Lucene Data/StandardAnalyzer";
-    public static final String INDEX_DIR_SIMPLE = "/home/amjad/Desktop/Lucene Data/SimpleAnalyzer";
-    public static final String INDEX_DIR_WHITE = "/home/amjad/Desktop/Lucene Data/WhiteAnalyzer";
+    public static final String INDEX_DIR_STANDARD = "/home/amjad/Desktop/Lucene Geo Location/StandardAnalyzer";
+    public static final String INDEX_DIR_SIMPLE = "/home/amjad/Desktop/Lucene Geo Location/SimpleAnalyzer";
+    public static final String INDEX_DIR_WHITE = "/home/amjad/Desktop/Lucene Geo Location/WhiteAnalyzer";
 
     public static final String ID = "id";
     public static final String COUNTRY = "country";
